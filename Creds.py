ef1710143155b5ea99aad0e63d45fa5b1a69c118
Alex Lazurka <alex.lazurka@gmail.com>
2016-11-26 13:06:01 -0500

    Create Creds.py

---
+++ Creds.py
@@ -0,0 +1,13 @@
+# login credentials to be used for Amazon MWS, database, etc.
+# Database:
+db_host='localhost'
+db_user='root'
+db_passwd='xxx'
+db_name='xxx'
+
+# MWS:
+sellerID= 'xxx'                                     # in MWS Scratchpad, 'SellerId'
+marketplaceID_US= 'ATVPDKIKX0DER'                   # in MWS Scratchpad, 'MarketplaceId', US marketplace
+dev_act_num= 'xxx'                                  # in MWS Scratchpad, 'MWSAuthToken:'
+aws_key= 'xxx'                                      # in MWS Scratchpad, 'AWSAccessKeyId:'
+s_key= 'xxx'                                        # in MWS Scratchpad, 'Secret Key:'
